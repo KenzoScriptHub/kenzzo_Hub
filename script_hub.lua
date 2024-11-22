01c1910ec1414f537401ccd431040a5f3a1d5a6e
Kenzo.Dz.Hub <anhtn1658@gmail.com>
2024-11-22 19:22:25 +0700

    Create script_hub.lua

---
+++ script_hub.lua
@@ -0,0 +1,6 @@
+Config = {
+    namescript = "Kenzo_Hub",--Tên Script
+    logoscript = "112548026510118",--ID Logo Script
+    tacgia = "Kenzo"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://scriptblox.com/raw/ScriptBlox-Direct-Execute-Feature_645", true))()
